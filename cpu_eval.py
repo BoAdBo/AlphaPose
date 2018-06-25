9189ebca33015e01a228c98316447617f3023a5f
king-kong <yangqp5@outlook.com>
2018-06-25 14:12:24 +0800

    adding cpu version as well as svm classifier

--- cpu_eval.py
+++ cpu_eval.py
@@ -19,6 +19,18 @@ from ssd.torchcv.models.ssd import SSD512, SSDBoxCoder
 from ssd.torchcv.visualizations import vis_image
 from pPose_nms import pose_nms, write_json
 
+# svm classification
+from sklearn import svm
+from sklearn.model_selection import cross_val_score, train_test_split, \
+    ShuffleSplit, LeaveOneOut, KFold, LeavePOut, GridSearchCV
+from sklearn.metrics import classification_report
+import numpy as np
+import json
+import argparse
+import random
+import pickle
+import itertools
+
 from opt import opt
 args = opt
 args.dataset = 'coco'
@@ -68,13 +80,12 @@ def inference_from_image(image):
     with torch.no_grad():
 
         # for resizing the bounding box
-        ht = inp.size(1)
-        wd = inp.size(2)
+        ht = inp.size(2)
+        wd = inp.size(3)
         # Human Detection
         img = Variable(img).cpu()
         print(img.size())
         loc_preds, cls_preds = det_model(img)
-        #print(img.size())
 
         boxes, labels, scores = box_coder.decode(ht, wd,
             loc_preds.data.squeeze().cpu(), F.softmax(cls_preds.squeeze(), dim=1).data.cpu())
@@ -115,7 +126,7 @@ def inference_from_image(image):
                   [res['bbox'] for res in result])
         drawCOCO(np.transpose(inp[0].data.numpy(), (1, 2, 0)), result)
 
-        print(result)
+        #print(result)
 
         result = {
             'result': result,
@@ -123,6 +134,44 @@ def inference_from_image(image):
         }
         #print(result)
         final_result.append(result)
+
+        # convert to numpy
+        # some redundant dimension
+        keyposes = [np.squeeze(x['keypoints'].numpy()) for x in result['result']]
+        scores = [x['kp_score'].numpy() for x in result['result']]
+
+        # normalize to within bbox
+        bboxes = np.array([x['bbox'].numpy() for x in result['result']])
+        assert(len(bboxes) == len(keyposes))
+        xmin, ymin, xmax, ymax = bboxes[:, 0], bboxes[:, 1], bboxes[:, 2], bboxes[:, 3]
+
+        # list of width and height
+        widths = xmax - xmin
+        heights = ymax - ymin
+
+        feature_list = list()
+
+        for i in range(len(keyposes)):
+            width = widths[i]
+            height = heights[i]
+
+            feature = np.array([])
+
+            # normalize it
+            for j in range(scores[i].shape[0]):
+                feature = np.append(feature, (keyposes[i][j][0] - xmin[i]) / width)
+                feature = np.append(feature, (keyposes[i][j][1] - ymin[i]) / height)
+                feature = np.append(feature, scores[i][j])
+
+            feature_list.append(feature)
+
+        feature_list = np.array(feature_list)
+
+        from sklearn.externals import joblib
+        clf = joblib.load('./models/svm/keypoint_recall.pkl')
+
+        rep = clf.predict_proba(feature_list)
+        print(rep)
 
     # TQDM
     print('Speed: {total:.2f} FPS | Num Poses: {pose} | Det time1, 2, 3: {det:.3f}, '.format(
@@ -141,7 +190,11 @@ def inference_from_image(image):
 # main function for testing
 if __name__ == '__main__':
     from PIL import Image
-    image = Image.open('examples/demo/1.jpg')
+    #image = Image.open('examples/demo/2.jpg')
+    image_location = '/home/king-kong/Downloads/storage/datasets/ai_challenger_dataset/ai_challenger_keypoint_train_20170902/ai_challenger_not_fight/08841929973516d175e1ce93962aa1aed5a50f9e.jpg'
+    image_location = 'examples/demo/1.jpg'
+
+    image = Image.open(image_location)
 
     if image.mode == 'L':
         image = imagel.convert('RGB')
